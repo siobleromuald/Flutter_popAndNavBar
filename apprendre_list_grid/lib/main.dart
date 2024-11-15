3239d818939c93d653c39b2cfe8238105391e347
siobleromuald <rsioble@gmail.com>
2024-11-15 22:37:44 +0100

    Apprendre les list et grid

---
+++ apprendre_list_grid/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:apprendre_list_grid/pages/accueil.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'List & grid',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Apprendre les listes'),
+    );
+  }
+}
+
+
